1f07f06fe5ecc4d339a069b5ce0784ce831a5b66
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-06-17 16:23:42 +0630

    layout fixes

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,11 @@ import Navbar from './Navbar';
 
 const Layout = (props) => {
   return (
-    <div className="bg-gray-50">
+    <div className=" min-h-screen bg-gray-50">
       <Header />
       <Navbar />
       {props.children}
-      <footer className="flex items-center justify-center absolute bottom-0 w-full py-4 border-t">
+      <footer className="flex items-center justify-center w-full py-4 border-t bg-gray-50">
         <p className="text-gray-500 text-xs md:text-sm">Made by mmt</p>
       </footer>
     </div>
